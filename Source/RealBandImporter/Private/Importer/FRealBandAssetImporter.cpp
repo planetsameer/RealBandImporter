96d35c9e0a48c0e0d6488514f4019aff3c676812
sameerkarjatkar <sameerkarjatkar@gmail.com>
2022-10-22 22:21:45 +0530

    Add files via upload
    Asset Search

--- Source/RealBandImporter/Private/Importer/FRealBandAssetImporter.cpp
+++ Source/RealBandImporter/Private/Importer/FRealBandAssetImporter.cpp
@@ -1,40 +1,142 @@
 
 #include "FRealBandAssetImporter.h"
-#include "GLTFAsset.h"
-#include "GLTFReader.h"
 
 
-bool FRealBandAssetGLTFImporter::OpenFile(const FString& InFileName)
+#include "UI/FRealBandStyle.h"
+#include "UI/FRealBandAssetLoader.h"
+//Assets
+#include "AssetRegistryModule.h"
+#include "AssetViewUtils.h"
+//Collections
+#include "CollectionManagerTypes.h"
+#include "CollectionManagerModule.h"
+#include "ICollectionManager.h"
+
+//core
+#include "CoreGlobals.h"
+#include "CoreMinimal.h"
+
+
+DEFINE_LOG_CATEGORY(LogCustom);
+
+
+#define LOCTEXT_NAMESPACE "RealBandImporter"
+
+void FRealBandAssetImporter::Init()
 {
-	GLTFReader->ReadFile(InFileName, false, true, *GLTFAsset);
-	const GLTF::FLogMessage* Found = GLTFReader->GetLogMessages().FindByPredicate(
-		[](const GLTF::FLogMessage& Message) { return Message.Get<0>() == GLTF::EMessageSeverity::Error; });
-	if (Found)
+	// Create Collections
+	//CreateCollection(FName("/Engine/RealBand"));
+
+	FString fPath = FPaths::ProjectSavedDir() / TEXT("Collections");
+
+	bool bCollection = false;
+	TArray<FCollectionNameType> collectionType;
+	if (FCollectionManagerModule::IsModuleAvailable())
 	{
-		return false;
-	}
-	check(GLTFAsset->ValidationCheck() == GLTF::FAsset::Valid);
+		//ICollectionManager& CollectionManager = FModuleManager::LoadModuleChecked<FCollectionManagerModule>("CollectionManager").Get();
+		ICollectionManager& CollectionManager = FCollectionManagerModule::GetModule().Get();
+		FPermissionListOwners pOwners;
+		CollectionManager.GetCollectionNames(ECollectionShareType::CST_Local, pOwners);
 
-	GLTFAsset->GenerateNames(FPaths::GetBaseFilename(InFileName));
 
-	// check extensions supported
-	static const TArray<GLTF::EExtension> SupportedExtensions = { GLTF::EExtension::KHR_MaterialsPbrSpecularGlossiness,
-																  GLTF::EExtension::KHR_MaterialsUnlit,
-																  GLTF::EExtension::KHR_LightsPunctual,
-																  GLTF::EExtension::KHR_MaterialsClearCoat,
-																  GLTF::EExtension::KHR_MaterialsTransmission,
-																  GLTF::EExtension::KHR_MaterialsSheen,
-																  GLTF::EExtension::KHR_MaterialsVariants,
-																  GLTF::EExtension::KHR_MaterialsSpecular,
-																  GLTF::EExtension::KHR_MaterialsIOR };
+		FAssetRegistryModule& AssetRegistryModule = FModuleManager::Get().LoadModuleChecked<FAssetRegistryModule>(TEXT("AssetRegistry"));
+		TArray<FAssetData> AssetDataList;
+		TArray<FString> APath;
+		APath.Add(TEXT("/Engine/RealBand"));
+		TArray<FName> PackageNames;
+		AssetViewUtils::GetAssetsInPaths(APath, AssetDataList);
+		TArray<FName> PackagePaths;
+		PackagePaths.Add(FName("/Engine/RealBand"));
 
-	for (GLTF::EExtension Extension : GLTFAsset->ExtensionsUsed)
-	{
-		if (SupportedExtensions.Find(Extension) == INDEX_NONE)
+		bool bAdded = false;
+		TArray<FAssetData> AssetsInPackages;
+		IAssetRegistry* AssetRegistry = &AssetRegistryModule.Get();
+		FARFilter Filter;
+		Filter.bIncludeOnlyOnDiskAssets = false;
+		Filter.PackagePaths = PackagePaths;
+		TArray<FAssetData> AsstInPackages;
+		AssetRegistry->GetAssets(Filter, AsstInPackages);
+		TSet<FName> ObjectPathsToAddToCollection;
+		TArray<FName> AssetClasses;
+		for (FAssetData& ObjectAsset : AsstInPackages)
 		{
-			LogMessages.Emplace(GLTF::EMessageSeverity::Warning, FString::Printf(TEXT("Extension is not supported: %s"), GLTF::ToString(Extension)));
+			AssetClasses.Add(ObjectAsset.GetClass()->GetFName());
+			ObjectPathsToAddToCollection.Add(ObjectAsset.ObjectPath);
+			
 		}
+
+		
+
+
+		//
+		//ICollectionManager& CollectionManager = FCollectionManagerModule::GetModule().Get();
+		TArray<FCollectionNameType> collectionsNames;
+		CollectionManager.GetCollections(collectionsNames);
+		FPermissionListOwners Owners;
+
+		bool bResult = CollectionManager.GetAssetsInCollection(FName("/Engine/RealBand"), ECollectionShareType::CST_Local, Owners);
+		bResult = CollectionManager.GetAssetsInCollection(FName("/Engine/RealBand"), ECollectionShareType::CST_Local, Owners);
+		if (!CollectionManager.CollectionExists(FName("RealBand"), ECollectionShareType::CST_All))
+		{
+			bCollection = CollectionManager.CreateCollection(FName("/Engine/RealBand"), ECollectionShareType::CST_Local, ECollectionStorageMode::Static);
+			if (bCollection)
+			{
+				FPermissionListOwners listOwners;
+				CollectionManager.GetCollectionNames(ECollectionShareType::CST_Local, listOwners);
+				FString Name = listOwners[0].GetPlainNameString();
+				FCollectionNameType collectObj(FName(Name), ECollectionShareType::CST_Local);
+				collectionType.Add(collectObj);
+				bool bSave = CollectionManager.SaveCollection(FName("RealBand"), ECollectionShareType::CST_Local);
+				if (!bSave)
+				{
+					FText Ferr = CollectionManager.GetLastError();
+					UE_LOG(LogCustom, Display, TEXT("%s"), *Ferr.ToString());
+
+				}
+			}
+		}
+
+
+		if (ObjectPathsToAddToCollection.Num() > 0)
+		{
+			bool bCollect = FCollectionManagerModule::GetModule().Get().AddToCollection(FName("RealBand"), ECollectionShareType::CST_Local, ObjectPathsToAddToCollection.Array());
+			if (bCollect)
+			{
+				if (!FCollectionManagerModule::GetModule().Get().SaveCollection(FName("RealBand"), ECollectionShareType::CST_Local))
+				{
+					UE_LOG(LogCustom, Display, TEXT("Failed to Save Collection"));
+				}
+			}
+		}
+	    // Initialize the Asset Viewer with the existing RealBand Assets
+	    // Get the Assets from the collections
+
+	    ConfigPicker.SelectionMode = ESelectionMode::Single;
+	    ConfigPicker.Filter.bRecursiveClasses = true;
+	    ConfigPicker.Filter.ClassNames = AssetClasses;
+	    ConfigPicker.Filter.bRecursivePaths = true;
+		
+	    //ConfigPicker.Filter.PackagePaths= PackagePaths;
+	    ConfigPicker.Collections = collectionType;
+	    ConfigPicker.InitialAssetViewType = EAssetViewType::Tile;
+	    ConfigPicker.bFocusSearchBoxWhenOpened = true;
+	    ConfigPicker.bAllowNullSelection = true;
+	    ConfigPicker.bAllowDragging = false;
+	    ConfigPicker.ThumbnailLabel = EThumbnailLabel::Type::AssetName;
+	   //ConfigPicker.AdditionalReferencingAssets = AssetData;
+	    ConfigPicker.bAutohideSearchBar = false;
+    }
+	else
+	{
+	    UE_LOG(LogCustom, Display, TEXT("Failed to Load Collection Manager Module"));
 	}
 
-	return true;
+
 }
+
+void FRealBandAssetImporter::GetAssetConfig(FAssetPickerConfig& oConfigPicker)
+{
+	oConfigPicker = ConfigPicker;
+}
+
+#undef LOCTEXT_NAMESPACE
--- Source/RealBandImporter/Private/Importer/FRealBandAssetImporter.h
+++ Source/RealBandImporter/Private/Importer/FRealBandAssetImporter.h
@@ -1,29 +1,17 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GLTFLogger.h"
 
-namespace GLTF
+#include "IContentBrowserSingleton.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogCustom, Log, All);
+
+class FRealBandAssetImporter : FNoncopyable
 {
-	class FFileReader;
-	struct FAsset;
-	class FMeshFactory;
-	class FMaterialFactory;
-}
-
-class FRealBandAssetGLTFImporter : FNoncopyable
-{
-	/** Returns the loaded asset from the last scene file. */
-	const GLTF::FAsset& GetAsset() const { return *GLTFAsset; }
-	/** Open and load a scene file. */
-	bool OpenFile(const FString& InFileName);
-	/** Returns any logged messages and clears them afterwards. */
-	const TArray<GLTF::FLogMessage>& GetLogMessages() const;
-
-
 private:
-	TUniquePtr<GLTF::FFileReader>               GLTFReader;
-	TUniquePtr<GLTF::FAsset>                    GLTFAsset;
-	mutable TArray<GLTF::FLogMessage>           LogMessages;
-
+	FAssetPickerConfig ConfigPicker;
+	
+public:
+	void GetAssetConfig(FAssetPickerConfig& oConfigPicker);
+	void Init();
 };
