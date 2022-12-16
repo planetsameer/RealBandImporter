97945b31a64f2fae096e0f0e598caac851665492
sameerkarjatkar <sameerkarjatkar@gmail.com>
2022-12-16 13:11:35 +0530

    Version 1.10

---
+++ Source/RealBandPluginGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RealBandPluginGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REALBANDPLUGIN_API ARealBandPluginGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
